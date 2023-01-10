0d49e029056a12c63d4190e1443459c46d276dcc
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2023-01-10 10:15:18 -0500

    fixed canvas for all topics

--- lessons/binarysearch/binarysearch.js
+++ lessons/binarysearch/binarysearch.js
@@ -8,7 +8,7 @@ function closeNav() {
 }
 
 function setup(){
-    var cnv = createCanvas(800, 500);
+    var cnv = createCanvas(1000, 550);
     var x = (windowWidth - width) / 2;
     var y = (windowHeight - height) / 2;
     cnv.position(x, y);
--- lessons/segtree/segtree.js
+++ lessons/segtree/segtree.js
@@ -6,3 +6,17 @@ function closeNav() {
     document.getElementById("navbar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+//lesson part
+function setup(){
+    var cnv = createCanvas(1000, 550);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+    background(205);
+}
+
+function draw(){
+    fill(0);
+    ellipse(100,100,100,100); 
+}
