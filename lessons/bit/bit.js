37ee5a36114cd980edec20f8111b9788732035e6
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2023-01-10 09:25:41 -0500

    p5 template canvas added to all lessons

--- lessons/bit/bit.js
+++ lessons/bit/bit.js
@@ -6,3 +6,18 @@ function closeNav() {
     document.getElementById("navbar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+//lesson portion
+function setup(){
+    var cnv = createCanvas(1200, 450);
+    var x = (windowWidth - width) / 2;
+    var y = 3 * (windowHeight - height) / 4;
+    cnv.position(x, y);
+    background(0);
+}
+
+function windowResized(){
+    width = windowWidth;
+    height = windowHeight;
+    resizeCanvas(width, height);
+}
