fc8f5045ceca2fe2472997ee369fbd00f0f4c7e0
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2023-01-12 09:23:32 -0500

    pushing for henry to copy

--- lessons/bit/bit.js
+++ lessons/bit/bit.js
@@ -7,6 +7,9 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
+//variable declaration
+let hoverColor = "DA2828";
+
 //lesson part
 function setup(){
     var cnv = createCanvas(1000, 550);
@@ -28,6 +31,19 @@ function draw(){
 
     text("Features", 25, 50);
     text("How it Works", 25, 250);
+
+    //feature descriptions
+    textSize(20);
+    text("- Can make prefix updates and queries", 25, 85);
+    text("- Unlike prefix sum array, is        ", 25, 135);
+    text("- Takes O(log N) per update / query", 25, 185);
+
+    FileList()
+
+    //how it works
+    text("- Can make prefix updates and queries", 25, 85);
+    text("- Unlike prefix sum array, is dynamic", 25, 135);
+    text("- Takes O(log N) per update / query", 25, 185);
 }
 
 
