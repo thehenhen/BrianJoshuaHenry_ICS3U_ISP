0f9a99820e1006d9d93f4a8f3d3f8324cec36b43
Henry Zhang <henry.zhang6@student.tdsb.on.ca>
2023-01-13 10:15:26 -0500

    more binary search

--- lessons/binarysearch/binarysearch.js
+++ lessons/binarysearch/binarysearch.js
@@ -1,3 +1,4 @@
+let stage=0;
 function openNav() {
     document.getElementById("navbar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -7,32 +8,10 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-function setup(){
-    var cnv = createCanvas(1000, 550);
-    var x = (windowWidth - width) / 2;
-    var y = (windowHeight - height) / 2;
-    cnv.position(x, y);
-    background(0);
-    rectMode(CENTER);
-
-    
-
-    textAlign(CENTER,CENTER);
-    textSize(20);
-    // 0 Start screen
-    // 1 How to find efficiently element in a sorted array: 
-    // 2 start at middle
-    //   Compare element with middle of array
-    //   If middle of array is smaller, then delete bottom half of array
-    //   Restart
-    // 3 practice using links
-}
-
-function draw(){
+function description(){
     background(255);
     //draws the border
     fill(255); 
-
     rect(500, 275, 995, 545);
 
     fill(0); 
@@ -57,4 +36,69 @@ function draw(){
     text("- If the middle element is larger, then cut the top half off and restart", 25, 385);
     text("- If the middle element is smaller, then cut the bottom half off and restart", 25, 435);
     text("- If the middle element is equal, then you've found it and you're done", 25, 485);
+}
+
+function show(){
+    rectMode(CENTER);
+    fill(255);
+    rect(400,240,40,40);
+    rect(440,240,40,40);
+    rect(480,240,40,40);
+    rect(520,240,40,40);
+    rect(560,240,40,40);
+    rect(600,240,40,40);
+    rect(640,240,40,40);
+    rect(680,240,40,40);
+    rect(720,240,40,40);
+    rect(760,240,40,40);
+    rect(800,240,40,40);
+    rect(840,240,40,40);
+    rect(880,240,40,40);
+    textAlign(CENTER,CENTER);
+    fill(0);
+    text("0",400,240);
+    text("1",440,240);
+    text("3",480,240);
+    text("4",520,240);
+    text("6",560,240);
+    text("7",600,240);
+    text("9",640,240);
+    text("10",680,240);
+    text("11",720,240);
+    text("14",760,240);
+    text("14",800,240);
+    text("15",840,240);
+    text("17",880,240);
+    if(stage==0){
+        text("Start",640,280);
+    }else if(stage==1){
+        
+    }
+    textAlign(LEFT,CENTER);
+}
+
+function setup(){
+    var cnv = createCanvas(1000, 550);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+    background(0);
+    rectMode(CENTER);
+
+    
+
+    textAlign(CENTER,CENTER);
+    textSize(20);
+    // 0 Start screen
+    // 1 How to find efficiently element in a sorted array: 
+    // 2 start at middle
+    //   Compare element with middle of array
+    //   If middle of array is smaller, then delete bottom half of array
+    //   Restart
+    // 3 practice using links
+}
+
+function draw(){
+    description();
+    show();
 }   
