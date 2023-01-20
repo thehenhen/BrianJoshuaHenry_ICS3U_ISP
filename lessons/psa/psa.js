573cdedf871c6a7aa2375a99f5ee8e77e85ce2d4
bsfaxious <86210813+bsfaxious@users.noreply.github.com>
2023-01-20 10:22:25 -0500

    i did it

--- lessons/psa/psa.js
+++ lessons/psa/psa.js
@@ -12,11 +12,12 @@ function setup(){
     var cnv = createCanvas(1000, 550);
     var x = (windowWidth - width) / 2;
     var y = (windowHeight - height) / 2;
-    cnv.position(x, y+50);
     background(205);
     rectMode(CENTER);
+    cnv.parent('sketch-holder'); 
 }
-
+let clicked = false;
+let clicked2 = false;
 function description(){
     fill(230);
     //draws the border
@@ -54,8 +55,18 @@ function show(){
     rectMode(CENTER);
     text("Starting array:", 630, 40);
     text("Difference array:", 630, 140)
+    if (clicked2) {
+    text("Final array: ",630,455);
+    textSize(14);
+    text("As visible, all elements between index that the operation was performed on ",25,485);
+    text("(inclusive) and the index that the operation was reversed on (non-inclusive) all increased by 10.",25,515);
+    textSize(25);
+    text("-->", 550,485);
+    textSize(20);
+    }
     stroke(0);
     fill(230);
+    
     rect(660,80,40,40);
     rect(700,80,40,40);
     rect(740,80,40,40);
@@ -73,7 +84,19 @@ function show(){
     rect(860,260,40,40);
     rect(900,260,40,40);
     rect(940,260,40,40);
-    
+
+    if (clicked2) {
+    rect(660,500,40,40);
+    fill("#FFFF00");
+    rect(700,500,40,40);
+    rect(740,500,40,40);
+    rect(780,500,40,40);
+    rect(820,500,40,40);
+    rect(860,500,40,40);
+    fill(230);
+    rect(900,500,40,40);
+    rect(940,500,40,40);
+    }
     
     fill(0);
     
@@ -95,9 +118,134 @@ function show(){
     text("-25",885,260);
     text("-4",930,260);
     
+    if(clicked2){
+    text("5",655,500);
+    text("22",690,500);
+    text("31",730,500);
+    text("29",770,500);
+    text("25",810,500);
+    text("47",850,500);
+    text("12",890,500);
+    text("8",935,500);
+    }
+
+    if (clicked) {
+        fill("#FF0000");
+        rect(700,260,40,40);
+        rect(900,260,40,40);
+        fill(255);
+        text("17",690,260);
+        text("-35",885,260);
+        
+
+    }
+    
+    fill(255);
+    
+    if(mouseX<570 && mouseX>480 && mouseY<250 && mouseY>210){
+        fill(200);
+    }
+    rect(530,230,100,40,5);
+    fill(0);
+    textSize(15);
+    text("Push changes",488,230);
+    textSize(20);
+    if(mouseX<680 && mouseX>640 && mouseY<280 && mouseY>240){
+        fill(0);
+        rect(660,260,40,40);
+        text("5 - 0 = 5", 660, 200);
+        fill(255);
+        text("5",655,260);
+        
+        
+    }
+    if(mouseX<720 && mouseX>680 && mouseY<280 && mouseY>240){
+        fill(0);
+        rect(700,260,40,40);
+        fill(255);
+        if (!clicked) {
+            text("7",695,260);
+            fill(0);
+            text("12 - 5 = 7", 660, 200);
+        }
+        if (clicked) {
+            text("17",69,260);
+            fill(0);
+            text("12 - 5 +      = 17", 660, 200);
+            fill("#FF0000")
+            text("10", 730, 200)
+            fill(255);
+            text("17",690,260);
+        }
+        
+        
+    }
+    if(mouseX<760 && mouseX>720 && mouseY<280 && mouseY>240){
+        fill(0);
+        rect(740,260,40,40);
+        text("21 - 12 = 9", 660, 200);
+        fill(255);
+        text("9",735,260);
+        
+    }
+    if(mouseX<800 && mouseX>760 && mouseY<280 && mouseY>240){
+        fill(0);
+        rect(780,260,40,40);
+        text("19 - 21 = -2", 660, 200);
+        fill(255);
+        text("-2",770,260);
+        
+    }
+    if(mouseX<840 && mouseX>800 && mouseY<280 && mouseY>240){
+        fill(0);
+        rect(820,260,40,40);
+        text("15 - 19 = -4", 660, 200);
+        fill(255);
+        text("-4",810,260);
+        
+    }
+    if(mouseX<880 && mouseX>840 && mouseY<280 && mouseY>240){
+        fill(0);
+        rect(860,260,40,40);
+        text("37 - 15 = 22", 660, 200);
+        fill(255);
+        text("22",850,260);
+        
+    }
+    if(mouseX<920 && mouseX>880 && mouseY<280 && mouseY>240){
+        fill(0);
+        rect(900,260,40,40);
+        
+        if (!clicked) {
+            fill(255);
+            text("-25",885,260);
+            fill(0);
+            text("12 - 37 = -25", 660, 200);
+        }
+        if (clicked) {
+            fill(0);
+            text("12 - 37 -      = -35", 660, 200);
+            fill("#FF0000")
+            text("10", 740, 200)
+            fill(255);
+            text("-35",885,260);
+        }
+        
+    }
+    if(mouseX<960 && mouseX>920 && mouseY<280 && mouseY>240){
+        fill(0);
+        rect(940,260,40,40);
+        text("8 - 12 = -4", 660, 200);
+        fill(255);
+        text("-4",930,260);
+        
+    }
+    
     
     textAlign(CENTER,CENTER);
     noStroke();
+
+    
     
     
 }
@@ -105,4 +253,14 @@ function show(){
 function draw(){
     description();
     show();
+}
+
+function mousePressed(){
+    if(mouseX<570 && mouseX>480 && mouseY<250 && mouseY>210){
+        clicked = !clicked;
+    }
+    if(mouseX<900 && mouseX>800 && mouseY<500 && mouseY>400){
+        clicked2 = true;
+    }
+        
 }
